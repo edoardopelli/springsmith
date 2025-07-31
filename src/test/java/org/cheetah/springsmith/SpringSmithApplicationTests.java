9b20917381c0eb7aadff26f8cf28c7d500da159d
Pelli, Edoardo Maria <edoardo.pelli@stefanini.com>
2025-07-31 17:19:24 +0200

    init repo

---
+++ src/test/java/org/cheetah/springsmith/SpringSmithApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cheetah.springsmith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSmithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
